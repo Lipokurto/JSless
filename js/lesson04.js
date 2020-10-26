95bc610f357860d1df513bbbef54ae9cb05aa68a
Lipokurto <lighting13@yandex.ru>
2020-10-26 20:36:14 +0300

    init commit

---
+++ js/lesson04.js
@@ -0,0 +1,14 @@
+function searchMain() {
+    let userText = document.getElementById('userData').value
+    let yandexCode = 'https://yandex.ru/search/?lr=146&text='
+    let fullTextSearch = yandexCode + userText
+    console.log(fullTextSearch)
+    // document.location.href = fullTextSearch
+    window.open(fullTextSearch,'_blank')
+}
+function color(bgc) {
+    document.body.style.background = bgc
+}
+function imageBg(source1) {
+    document.body.style.backgroundImage = "url('"+source1+"')"
+}
